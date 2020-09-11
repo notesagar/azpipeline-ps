336d643a60c243ec07a7b6603ba1a542a52c8e7c
notesagar <70768081+notesagar@users.noreply.github.com>
2020-09-11 22:55:43 +0530

    Update myScript.ps1

--- myScript.ps1
+++ myScript.ps1
@@ -1,5 +1,4 @@
 write-host "Hi ! This is Sagar's 1st AZ Pipeline for executing powershell script"
-write-host "File editedand job run on:"
+write-host "with CI mode enabled"
 date
 $env:computername
-ps
